163019ee6e29cdfce73e43f2b7aa14c73c56b81c
Alex Alext <alextoma8032002@yahoo.com>
2023-12-22 18:39:25 +0200

    Add project files.

---
+++ Toma_Alexandru_Lab11/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Toma_Alexandru_Lab11.Data;
+
+
+namespace Toma_Alexandru_Lab11
+
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+
+        }
+    }
+}
